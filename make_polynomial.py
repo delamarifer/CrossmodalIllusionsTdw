8ea15e782cc2de5997b6d6bd4f3a395bbe5b9715
delamarifer <delamarifer@gmail.com>
2022-10-12 14:16:48 -0400

    _

--- make_polynomial.py
+++ make_polynomial.py
@@ -34,35 +34,59 @@ def get_poly_velocity(path_len, discont_len):
     return [x for x in range(len(new_y))], new_y
 
 
+# def get_poly_velocity2(path_len,discont_len):
+#     a = 3.4936*(10**-7)
+#     b = -0.0000258902
+#     c = 0.000796324
+#     d = -0.0131828
+#     f = 0.12722
+#     g = -0.72374
+#     h = 2.33763
+#     i = -3.97475
+#     j = 5.5485 
+#     k = -0.0490286
+#     if discont_len == 0:
+#         x = np.linspace(0.1,17.4,path_len)
+#         y = a*(x**9) + b*(x**8) + c*(x**7) + d*(x**6) + f*(x**5) + g*(x**4) + h*(x**3) + i*(x**2) + j*(x**1) + k
+#         y = 0.07*y
+#         new_y = -0.7*y+1.5
+#     else:
+#         x = np.linspace(0.1,17.4,path_len)
+#         y = a*(x**9) + b*(x**8) + c*(x**7) + d*(x**6) + f*(x**5) + g*(x**4) + h*(x**3) + i*(x**2) + j*(x**1) + k
+#         mid_arr = np.repeat([0.0001],discont_len)
+#         y = 0.07*y
+#         new_y = np.hstack(( y,mid_arr,y)).ravel()    
+#     return  new_y
+   
+
 def get_poly_velocity2(path_len,discont_len):
-    a = 3.4936*(10**-7)
-    b = -0.0000258902
-    c = 0.000796324
-    d = -0.0131828
-    f = 0.12722
-    g = -0.72374
-    h = 2.33763
-    i = -3.97475
-    j = 5.5485 
-    k = -0.0490286
+    a = 0.0000205326
+    b = -0.00426021
+    c = 0.0800616
+    d = -0.510542
+    f = 0.990328
+    g = 0.536806
+   
     if discont_len == 0:
-        x = np.linspace(0.1,17.4,path_len)
-        y = a*(x**9) + b*(x**8) + c*(x**7) + d*(x**6) + f*(x**5) + g*(x**4) + h*(x**3) + i*(x**2) + j*(x**1) + k
+        x = np.linspace(-0.1,5.2,path_len)
+        y = a*(x**5) + b*(x**4) + c*(x**3) + d*(x**2) + f*(x**1) + g
         y = 0.07*y
         new_y = -0.7*y+1.5
     else:
-        x = np.linspace(0.1,17.4,path_len)
-        y = a*(x**9) + b*(x**8) + c*(x**7) + d*(x**6) + f*(x**5) + g*(x**4) + h*(x**3) + i*(x**2) + j*(x**1) + k
+        x = np.linspace(1,5.2,path_len)
+        y = a*(x**5) + b*(x**4) + c*(x**3) + d*(x**2) + f*(x**1) + g
         mid_arr = np.repeat([0.0001],discont_len)
-        y = 0.07*y
-        new_y = np.hstack(( y,mid_arr,y)).ravel()    
+        # y = 0.07*y
+        x = np.linspace(-0.1,5.2,path_len)
+        y2 = a*(x**5) + b*(x**4) + c*(x**3) + d*(x**2) + f*(x**1) + g
+        # y2 = 0.07*y2
+        new_y = np.hstack(( y,mid_arr,y2)).ravel()    
     return  new_y
-   
 
 
-# y = get_poly_velocity2(60,0)
+# y = get_poly_velocity2(30,4)
 # x = [x for x in range(len(y))]
-# plot_poly(x,-0.7*y+1.5)
+# plot_poly(x,y)
 
 # print(np.size(y))
 
